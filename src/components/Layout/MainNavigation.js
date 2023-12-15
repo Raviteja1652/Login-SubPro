116394ecd2779754ffa06227d34b8b1b104a93fc
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-15 10:24:39 +0530

    Logging out and redirecting

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  const history = useHistory()
   const ctx = useContext(AuthContext);
   const isLoggedIn = ctx.isLoggedIn;
   const logoutHandler = () => {
     ctx.logout()
+    history.replace('/auth')
   }
   return (
     <header className={classes.header}>
